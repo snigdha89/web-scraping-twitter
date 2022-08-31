7926760520be78ed31e440abfa3ba0642f9d189b
Snigdha Dubey <83393290+snigdha89@users.noreply.github.com>
2022-08-31 19:58:39 -0400

    Update webscraping_twitter.py

--- webscraping_twitter.py
+++ webscraping_twitter.py
@@ -1,17 +1,9 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 import twint
 import nest_asyncio
+import pandas as pd 
+import matplotlib.pyplot as plt
+
 nest_asyncio.apply()
-
-
-# In[2]:
-
-
 # Configure
 c = twint.Config()
 c.Search = "\"Booster shot\"" "Omicron"
@@ -21,19 +13,8 @@ c.Lang = "en"
 c.Limit = 300
 twint.run.Search(c)
 
-
-# In[3]:
-
-
-import pandas as pd 
-import matplotlib.pyplot as plt
 data = pd.read_csv("Desktop/Assignment1.csv")
 final = data['tweet'].str.lower()
-
-
-# In[4]:
-
-
 omicron = []
 se = []
 booster = []
@@ -47,11 +28,6 @@ s_o = sum(omicron)
 s_se = sum(se)
 s_b = sum(booster)
 s_v = sum(vaccine)
-
-
-# In[5]:
-
-
 x = ('side effect','omicron' , 'booster', 'vaccine' )
 y = (s_se,s_o,s_b, s_v)
 plt.bar(x,y,align='center') # A bar chart
@@ -60,16 +36,4 @@ plt.ylabel('Frequency')
 plt.grid(axis='y', alpha=0.50)
 plt.title('Word Frequency Histogram')
 plt.show()
-
-
-# In[6]:
-
-
 print(final)
-
-
-# In[ ]:
-
-
-
-
